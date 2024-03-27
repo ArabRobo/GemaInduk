df8bbc97205a10d216a5434562316a5eed17d0c3
ArabRobo <121577164+ArabRobo@users.noreply.github.com>
2024-03-28 00:29:22 +0700

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 DB_URI = os.environ.get("DATABASE_URL", "postgres://kfnpbltj:8HA2oTrv95OD2hX5tMmbNeIuQK7Q3QYz@stampy.db.elephantsql.com/kfnpbltj")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001246330125"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002065900038"))
 FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1001814641743"))
 
 # Tulisan joinnya mau gimana
